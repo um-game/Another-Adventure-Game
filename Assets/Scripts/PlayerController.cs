fe95aad564cb77a15872705ca9ab2e28cebc93fb
Adam Clemons <nohbodyz@gmail.com>
2018-02-27 06:38:18 -0700

    Player Movement
    Connects player movement states properly.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,17 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float maxSpeed = 10f;
+    public float maxSpeed;
 
     Animator anim;
+    Rigidbody2D rb2d;
     
 
     // Use this for initialization
     void Start () {
-
         anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
         anim.SetInteger("direction", 3);
         anim.SetBool("moving", false);
+
 	}
 	
 	// Update is called once per frame
@@ -24,34 +26,36 @@ public class PlayerController : MonoBehaviour {
         float moveY = Input.GetAxis("Vertical");
 
         
-        if (Input.GetKeyDown(KeyCode.W)) {
+        if (Input.GetKey(KeyCode.W)) {
             anim.SetInteger("direction", 1);
             anim.SetBool("moving", true);
 
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, moveY * maxSpeed);
+           rb2d.velocity = new Vector2(0, moveY * maxSpeed);
         }
-        else if (Input.GetKeyDown(KeyCode.S)) {
+        else if (Input.GetKey(KeyCode.S)) {
             anim.SetInteger("direction", 3);
             anim.SetBool("moving", true);
 
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, moveY * maxSpeed);
+           rb2d.velocity = new Vector2(0, moveY * maxSpeed);
         }
-        else if (Input.GetKeyDown(KeyCode.D)) {
+        else if (Input.GetKey(KeyCode.D)) {
             anim.SetInteger("direction", 2);
             anim.SetBool("moving", true);
 
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * maxSpeed, 0);
+           rb2d.velocity = new Vector2(moveX * maxSpeed, 0);
         }
-        else if (Input.GetKeyDown(KeyCode.A)) {
+        else if (Input.GetKey(KeyCode.A)) {
             anim.SetInteger("direction", 4);
             anim.SetBool("moving", true);
 
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveX * maxSpeed, 0);
+            rb2d.velocity = new Vector2(moveX * maxSpeed, 0);
         }
         
-        if (!Input.GetKeyDown(KeyCode.A) && !Input.GetKeyDown(KeyCode.S) && !Input.GetKeyDown(KeyCode.D) && !Input.GetKeyDown(KeyCode.W)) {
+        else {
             anim.SetBool("moving", false);
+            rb2d.velocity = new Vector2(0, 0);
         }
+        
         
     }
 }
