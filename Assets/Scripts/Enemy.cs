1d25a58289885b0c8a709428e6990f0fffba3b3e
Cameron <cameron.gomke@umontana.edu>
2018-03-13 13:55:06 -0600

    Enemy
    Enemy disappears

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	void OnTriggerEnter2D(Collider2D other)
+    {
+    Debug.Log (other.gameObject.name);
+        Destroy(this.gameObject);  
+    }
+	
+
+}
