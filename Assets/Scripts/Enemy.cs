e165a82ace23085d778b47838eba923350a5e415
Adam Clemons <nohbodyz@gmail.com>
2018-03-13 21:14:13 -0600

    Adds boundaries to walls
    Create Walls edges. Fixes issue with items being created when the scene was destroyed (instead of the enemy just dying).

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class Enemy : MonoBehaviour {
 
 	public worldItem item; // Type of item enemy will drop
+    bool dying = false;
 
 	// Use this for initialization
 	void Start () {
@@ -18,12 +19,14 @@ public class Enemy : MonoBehaviour {
 
 	void OnTriggerEnter2D(Collider2D collision) {
 		this.gameObject.SetActive (false);
+        dying = true;
 		Destroy (this.gameObject);
 	}
 
     private void OnDestroy()
     {
-        Instantiate(item, transform.position, item.transform.rotation);
+        if (dying)
+            Instantiate(item, transform.position, item.transform.rotation);
         //item.transform.position = new Vector3 (this.gameObject.transform.position.x, this.gameObject.transform.position.y + 1, 0);
     }
 }
