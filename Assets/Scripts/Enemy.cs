7f5779cd1bcb12642cf545c62c948dc4525ee654
Luckek1354 <luckek1354@gmail.com>
2018-03-29 15:56:32 -0600

    Added very basic enemy movement
    enemy will now move towards player  if player is within a certain range

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,21 +6,45 @@ public class Enemy : MonoBehaviour {
 
 	public worldItem item; // Type of item enemy will drop
     bool dying = false;
+	Rigidbody2D rb2d;
+	int maxSpeed = 1;
+	int range; // If player is within range, enemy will attack
+	Transform p; // Hold transform of player, used to calculate movement direction
 
 	// Use this for initialization
 	void Start () {
-		
+		rb2d = GetComponent<Rigidbody2D> ();
+		p = GameObject.Find ("player").transform;
+
+		range = 2;
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
+	void FixedUpdate () {
+
+		Vector3 diff = p.transform.position - this.transform.position; // Get difference in position
+
+		if (diff.magnitude < range) {
+
+			diff.Normalize ();
+
+			// Move towards player
+			rb2d.velocity = new Vector2 (diff.x * maxSpeed, diff.y * maxSpeed);
+
+		} else {
+			rb2d.velocity = new Vector2 (0, 0); // Otherwse we stop moving
+		}
 	}
 
-	void OnTriggerEnter2D(Collider2D collision) {
-		this.gameObject.SetActive (false);
-        dying = true;
-		Destroy (this.gameObject);
+	void OnCollisionEnter2D(Collision2D collision) {
+
+		Player p = collision.gameObject.GetComponent<Player> ();
+
+		if (p != null) {
+			this.gameObject.SetActive (false);
+			dying = true;
+			Destroy (this.gameObject);
+		}
 	}
 
     private void OnDestroy()
