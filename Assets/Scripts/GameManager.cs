68081de5bd5a82abc214258e100561459537a89d
Zachary Falkner <falkzach@gmail.com>
2018-04-11 13:20:29 -0600

    creates grid, commiting before i break it

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+	public static GameManager instance = null;
+	public BoardManager boardScript;
+
+	private bool doingSetup;
+
+	// Use this for initialization
+	void Awake () {
+		if (instance == null)
+			instance = this;
+		else if (instance != this)
+			Destroy (gameObject);
+
+		DontDestroyOnLoad (gameObject);
+		boardScript = GetComponent<BoardManager> ();
+		InitGame ();
+	}
+
+	void OnLevelWasLoaded (int index)
+	{
+		InitGame ();
+	}
+
+	void InitGame()
+	{
+		doingSetup = true;
+
+		boardScript.SetupScene (0);//TODO: random?
+	}
+
+	public void GameOver()
+	{
+		enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (doingSetup)
+			return;
+
+	}
+
+
+}
