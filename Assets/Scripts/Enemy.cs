107b60eea7e7152ee39d820605b6f2cc92e1a915
Cameron <cameron.gomke@umontana.edu>
2018-04-11 20:18:26 -0600

    working on movement

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,31 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemy : MonoBehaviour {
-
-	public worldItem item; // Type of item enemy will drop
+// TODO: put common fucntionallity of enemy and payer into parent class
+public class Enemy : MonoBehaviour
+{
+    public worldItem item; // Type of item enemy will drop
     bool dying = false;
+    Rigidbody2D rb2d;
+    int maxSpeed = 1;
+    int range; // If player is within range, enemy will attack
+    Transform p; // Hold transform of player, used to calculate movement direction
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // Use this for initialization
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        p = GameObject.Find("player").transform;
 
-	void OnTriggerEnter2D(Collider2D collision) {
-		this.gameObject.SetActive (false);
-        dying = true;
-		Destroy (this.gameObject);
-	}
+        range = 10;
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        // FIXME: enemy gets stuck
+        Vector3 diff = p.transform.position - this.transform.position; // Get difference in position
+
+        if (diff.magnitude < range)
+        {
+            diff.Normalize();
+
+            // Move towards player
+            rb2d.velocity = new Vector2(diff.x * maxSpeed, diff.y * maxSpeed);
+
+        }
+        else
+        {
+            rb2d.velocity = new Vector2(0, 0); // Otherwse we stop moving
+        }
+    }
+
+
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+       
+
+        Player p = collision.gameObject.GetComponent<Player>();
+
+        if (p != null)
+        {
+            this.gameObject.SetActive(false);
+            dying = true;
+            Destroy(this.gameObject);
+        }
+    }
+
 
     private void OnDestroy()
     {
         if (dying)
             Instantiate(item, transform.position, item.transform.rotation);
-        //item.transform.position = new Vector3 (this.gameObject.transform.position.x, this.gameObject.transform.position.y + 1, 0);
     }
 }
