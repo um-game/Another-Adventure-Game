d9c1754ac231917b094bf0c9ff22f635b8488e8f
Luckek1354 <luckek1354@gmail.com>
2018-04-22 19:11:22 -0600

    Created RNG
    Created random number generator capable of utilizing probabilities.

---
+++ Assets/Scripts/RandomNumberGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomNumberGenerator {
+
+	private float total;
+	private float[] probs;
+
+	public RandomNumberGenerator(float[] probs) {
+
+		foreach (float elem in probs) {
+			total += elem;
+		}
+
+		// C# copies arrays automatically
+		this.probs = probs;
+
+		printProbs ();
+	}
+
+	public int next () {
+
+		float randomPoint = Random.value * total;
+
+		for (int i= 0; i < probs.Length; i++) {
+			if (randomPoint < probs[i]) {
+				return i;
+			}
+			else {
+				randomPoint -= probs[i];
+			}
+		}
+		return probs.Length - 1;
+	}
+
+	private void printProbs() {
+		foreach (float elem in this.probs) {
+			Debug.Log (elem);
+		}
+	}
+}
