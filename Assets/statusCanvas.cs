e31db0cb775b4d1b69e2c3039b3774db2a68ad70
Luckek1354 <luckek1354@gmail.com>
2018-04-25 12:01:54 -0600

    Added status canvas persistence

---
+++ Assets/statusCanvas.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class statusCanvas : MonoBehaviour {
+
+	public static statusCanvas myCanvas;
+
+	// Use this for initialization
+	void Start () {
+
+		if (myCanvas == null) {
+			myCanvas = this;
+		} else if (myCanvas != this) {
+			Destroy (gameObject);
+		}
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
