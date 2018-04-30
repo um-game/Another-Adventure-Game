d7c3d80565d64027c0084a97ab6ba3a3573d208e
Adam Clemons <nohbodyz@gmail.com>
2018-04-30 14:06:31 -0600

    Fixed dungeon not regenerating bug.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,19 +4,12 @@ using System.Collections.Generic;
 using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
-
-	public static GameManager instance = null;
+    
 
 	private bool doingSetup;
 
 	// Use this for initialization
 	void Awake () {
-		if (instance == null)
-			instance = this;
-		else if (instance != this)
-			Destroy (gameObject);
-
-		DontDestroyOnLoad (gameObject);
 		InitGame ();
 	}
 
