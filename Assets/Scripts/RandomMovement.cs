b3d469b628cddd27f0705aff6bbfc36a4f7da43a
Zachary Falkner <falkzach@gmail.com>
2018-04-30 17:25:09 -0600

    blade trap insanity

---
+++ Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomMovement : MonoBehaviour {
+
+    public float acceleration;
+    public float deceleration;
+
+    private Rigidbody2D rb2d;
+    private Vector2 input;
+
+    // Use this for initialization
+    void Start () {
+        rb2d = GetComponent<Rigidbody2D>();
+        rb2d.drag = deceleration;
+    }
+
+    void Update()
+    {
+        input.x = Input.GetAxis("Horizontal");
+        input.y = Input.GetAxis("Vertical");
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate() {
+	    rb2d.AddForce(-1 * input * acceleration * Time.deltaTime, ForceMode2D.Impulse);
+    }
+}
